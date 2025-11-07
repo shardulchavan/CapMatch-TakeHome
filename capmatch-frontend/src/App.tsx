6eeee04de9aa84e538447d02e593289a6b9db7d1
shardulchavan <chavan.shardul360@gmail.com>
2025-11-07 07:32:00 -0500

    Deploying Frontend v4

--- capmatch-frontend/src/App.tsx
+++ capmatch-frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Chart from 'chart.js/auto';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchPage from './components/SearchPage';
 import DemographicsDetail from './pages/DemographicsDetail';
